8d78e9771e7d5373fe0dce3bf309ac047da8639e
Sergiy <37875819+Shkum@users.noreply.github.com>
2022-09-16 23:35:33 +0200

    Initial commit

---
+++ movie_app/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Movie(models.Model):
+    name = models.CharField(max_length=40)
+    rating = models.IntegerField()
+    year = models.IntegerField()
+    budget = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.name} - {self.rating}'
+
+
