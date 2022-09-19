746798fa73fae739f6047331dd40d3fe2ddc6b52
Sergiy <37875819+Shkum@users.noreply.github.com>
2022-09-19 10:54:10 +0200

    update

--- movie_app/views.py
+++ movie_app/views.py
@@ -4,8 +4,14 @@ from django.shortcuts import render
 from . models import Movie
 
 def show_all_movie(request):
-    movies =Movie.objects.all()
+    movies = Movie.objects.all()
     return render(request, 'movie_app/all_movies.html', {
         'movies': movies
     })
 
+
+def show_one_movie(request, id_movie: int):
+    movie = Movie.objects.get(id=id_movie)
+    return render(request, 'movie_app/one_movie.html', {
+        'movie': movie
+    })
